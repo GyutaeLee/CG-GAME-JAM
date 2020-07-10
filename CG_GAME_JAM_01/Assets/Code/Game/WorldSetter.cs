c25a38077c429130bec7051b1ef9e6bea5d6ae60
Chanhaeng Lee <lch32111@naver.com>
2020-07-11 02:14:43 +0900

    - feat : 게임 물리를 위한 테스트데모 제작 중

---
+++ CG_GAME_JAM_01/Assets/Code/Game/WorldSetter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 현재 테스트 용도로, 월드에 필요한 오브젝트들을 세팅한다.
+ */
+public class WorldSetter : MonoBehaviour
+{
+    Light[] m_cubeLights = new Light[6];
+    Transform m_cubeWorldTransform;
+    GameObject m_cubeWorldGameObject;
+
+    GameObject[] m_prefabPlaneArea = new GameObject[12];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_cubeWorldTransform = transform.Find("CubeWorld");
+        m_cubeWorldGameObject = m_cubeWorldTransform.gameObject;
+
+        GameObject prefab = Resources.Load("Prefab/pAreaPlane") as GameObject;
+        
+        for(int i = 0; i < 12; ++i)
+        {
+            m_prefabPlaneArea[i] = GameObject.Instantiate(prefab, m_cubeWorldTransform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
