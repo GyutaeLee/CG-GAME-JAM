94f65c9802187f4296b4690071150d4195976164
GyutaeLee <gyutae07@gmail.com>
2020-07-12 00:46:22 +0900

    - feat : ThrowableObject 수정 1) 리셋 함수 추가

--- CG_GAME_JAM_01/Assets/Code/CGObject/CGThrowableObject.cs
+++ CG_GAME_JAM_01/Assets/Code/CGObject/CGThrowableObject.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CGThrowableObject : CGObject
 {
+    private Transform m_OriginTransform;
+
     private void Start()
     {
         InitializeCGThrowableObject();
@@ -12,5 +14,17 @@ public class CGThrowableObject : CGObject
     private void InitializeCGThrowableObject()
     {
         IsMovable = true;
+
+        m_OriginTransform = transform;
+    }
+
+    public void ResetObject()
+    {
+        transform.position = m_OriginTransform.position;
+        transform.rotation = m_OriginTransform.rotation;
+        transform.localScale = m_OriginTransform.localScale;
+
+        transform.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        transform.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
     }
 }
