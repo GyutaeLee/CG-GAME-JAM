741b359006a7dfbe6782825206eda9c72b115684
GyutaeLee <gyutae07@gmail.com>
2020-07-11 21:46:43 +0900

    - fix : 플레이어 회전 기능 수정 1) 각 사분면에서 회전할 수 있도록 수정 2) 회전 방법 변경 : vector -> angle

--- CG_GAME_JAM_01/Assets/Code/Game/Player.cs
+++ CG_GAME_JAM_01/Assets/Code/Game/Player.cs
@@ -10,9 +10,12 @@ public class Player : MonoBehaviour
 
     public float playerMoveSpeed = 0.01f;
 
-    private const int areaCount = 6;
-    public Vector3[] forwardVectors;
-    public Vector3[] rightVectors;
+    private const int m_AreaCount = 6;
+    private Vector3[] m_MoveForwardVectors;
+    private Vector3[] m_MoveRightVectors;
+
+    private Vector3 m_PlayerUpVector;
+    private Quaternion m_OriginQuaternion;
 
     private void Start()
     {
@@ -22,21 +25,50 @@ public class Player : MonoBehaviour
     private void InitializePlayer()
     {
         SetDirectionVectors();
+
+        m_PlayerUpVector = PlayerObject.transform.up;
+        m_OriginQuaternion = GetComponent<Transform>().localRotation;
     }
 
     private void SetDirectionVectors()
     {
-        forwardVectors = new Vector3[areaCount];
-        rightVectors = new Vector3[areaCount];
+        m_MoveForwardVectors = new Vector3[m_AreaCount];
+        m_MoveRightVectors = new Vector3[m_AreaCount];
 
-        forwardVectors[(int)WorldSetter.CubeAreaEnum.YP] = Vector3.forward;
-        rightVectors[(int)WorldSetter.CubeAreaEnum.YP] = Vector3.right;
+        m_MoveForwardVectors[(int)WorldSetter.CubeAreaEnum.YP] = Vector3.forward;
+        m_MoveRightVectors[(int)WorldSetter.CubeAreaEnum.YP] = Vector3.right;
 
-        forwardVectors[(int)WorldSetter.CubeAreaEnum.ZM] = Vector3.up;
-        rightVectors[(int)WorldSetter.CubeAreaEnum.ZM] = Vector3.right;
+        m_MoveForwardVectors[(int)WorldSetter.CubeAreaEnum.ZM] = Vector3.up;
+        m_MoveRightVectors[(int)WorldSetter.CubeAreaEnum.ZM] = Vector3.right;
     }
 
+    private float AddQuadrantValue(float x, float y)
+    {
+        float quadrantValue = 0.0f;
 
+        if (x > 0 && y > 0)
+        {
+            quadrantValue = 0.0f;
+        }
+        else if (x < 0 && y > 0)
+        {
+            quadrantValue = -180.0f;
+        }
+        else if (x < 0 && y < 0)
+        {
+            quadrantValue = -180.0f;
+        }
+        else if (x > 0 && y < 0)
+        {
+            quadrantValue = 0.0f;
+        }
+
+        return quadrantValue;
+    }
+
+    /*
+     *  PLAYER CONTROL
+     */
     public void MovePlayerObject(float horizontalValue, float verticalValue)    
     {
         if (horizontalValue == 0 && verticalValue == 0)
@@ -46,8 +78,8 @@ public class Player : MonoBehaviour
         float verticalSpeed   = verticalValue * playerMoveSpeed;
         Vector3 moveVector = Vector3.zero;
 
-        moveVector += rightVectors[(int)_GameManager.PlayerCubeAreaEnum] * horizontalSpeed;
-        moveVector += forwardVectors[(int)_GameManager.PlayerCubeAreaEnum] * verticalSpeed;
+        moveVector += m_MoveRightVectors[(int)_GameManager.PlayerCubeAreaEnum] * horizontalSpeed;
+        moveVector += m_MoveForwardVectors[(int)_GameManager.PlayerCubeAreaEnum] * verticalSpeed;
 
         PlayerRigidbody.AddForce(moveVector);
     }
@@ -57,12 +89,21 @@ public class Player : MonoBehaviour
         if (horizontalValue == 0 && verticalValue == 0)
             return;
 
-        Vector3 rotateVector = Vector3.zero;
+        float rotateAngle = 0.0f;
 
-        rotateVector += Vector3.right * horizontalValue;
-        rotateVector += Vector3.forward * verticalValue;
+        // 조이스틱 degree 계산
+        rotateAngle = Mathf.Atan(verticalValue / horizontalValue);
+        rotateAngle *= (float)(180.0f / Mathf.PI);
 
-        Quaternion quaternion = Quaternion.LookRotation(rotateVector);
-        PlayerObject.transform.rotation = quaternion;
+        // 탄젠트 주기에 따른 가중치 계산
+        rotateAngle += AddQuadrantValue(horizontalValue, verticalValue);
+
+        // 큐브 오브젝트 위치에 맞게 -90 가중치 계산
+        rotateAngle -= 90.0f;
+
+        // 플레이어 오브젝트의 up 벡터를 기준으로 절대 각도 '-' (반시계방향으로 변경) 회전
+        PlayerObject.transform.rotation = Quaternion.AngleAxis(-rotateAngle , m_PlayerUpVector) * m_OriginQuaternion;
     }
 }
+
+//?? 규태 : normalize 및 Time.deltaTime 하기
