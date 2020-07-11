7000fef852db4a0706978dc89c163735f5283a16
GyutaeLee <gyutae07@gmail.com>
2020-07-11 23:16:03 +0900

    - feat : EObjectState 추가

--- CG_GAME_JAM_01/Assets/Code/Game/CGObject.cs
+++ CG_GAME_JAM_01/Assets/Code/Game/CGObject.cs
@@ -6,12 +6,42 @@ public class CGObject : MonoBehaviour
 {
     public enum EObjectType
     { 
-        OBJECT_TYPE_NONE = 0,
+        OBJECT_TYPE_NONE    = 0,
         OBJECT_TYPE_MOVABLE,
         OBJECT_TYPE_UNMOVABLE,
 
         MAX_OBJECT_TYPE
     }
 
+    public enum EObjectState
+    {
+        OBJECT_STATE_NONE   = 0,
+        OBJECT_STATE_GROUND,
+        OBJECT_STATE_PICKED,
+        OBJECT_STATE_THROWN,
+        OBJECT_STATE_ROLLING,
+
+        MAX_OBJECT_STATE
+    }
+
     public EObjectType ObjectType;
+    public EObjectState ObjectState;
+
+    public bool IsMovable()
+    {
+        bool isMovable = false;
+
+        isMovable = (ObjectType == EObjectType.OBJECT_TYPE_MOVABLE);
+
+        return isMovable;
+    }
+
+    public bool IsPickable()
+    {
+        bool isPickable = false;
+
+        isPickable = (ObjectState == EObjectState.OBJECT_STATE_GROUND);
+
+        return isPickable;
+    }
 }
