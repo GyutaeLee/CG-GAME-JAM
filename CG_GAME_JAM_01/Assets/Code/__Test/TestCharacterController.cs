c206e7252aa872b7e25169952090a0d29245b0d7
Chanhaeng Lee <lch32111@naver.com>
2020-08-23 18:57:41 +0900

    - fix : rename _Test into __Test          finish implementing 3rd person camera with camera collision

--- CG_GAME_JAM_01/Assets/Code/__Test/TestCharacterController.cs
+++ CG_GAME_JAM_01/Assets/Code/__Test/TestCharacterController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class TestCharacterController : MonoBehaviour
 {
-    public float characterSpeed;
+    public float characterMoveSpeed;
+    public float characterRotateSpeed;
 
     private Transform m_transform;
     private CharacterController m_characterController;
@@ -29,22 +30,32 @@ public class TestCharacterController : MonoBehaviour
 
         if(Input.GetKey(KeyCode.W) == true)
         {
-            m_characterController.Move(transform.forward * Time.deltaTime * characterSpeed);
+            m_characterController.Move(transform.forward * Time.deltaTime * characterMoveSpeed);
         }
 
         if (Input.GetKey(KeyCode.A) == true)
         {
-            m_characterController.Move(transform.right * -1.0f * Time.deltaTime * characterSpeed);
+            m_characterController.Move(transform.right * -1.0f * Time.deltaTime * characterMoveSpeed);
         }
 
         if (Input.GetKey(KeyCode.S) == true)
         {
-            m_characterController.Move(transform.forward * -1.0f * Time.deltaTime * characterSpeed);
+            m_characterController.Move(transform.forward * -1.0f * Time.deltaTime * characterMoveSpeed);
         }
 
         if (Input.GetKey(KeyCode.D) == true)
         {
-            m_characterController.Move(transform.right * Time.deltaTime * characterSpeed);
+            m_characterController.Move(transform.right * Time.deltaTime * characterMoveSpeed);
+        }
+
+        if(Input.GetKey(KeyCode.Q) == true)
+        {
+            m_transform.Rotate(m_transform.up, -characterRotateSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.E) == true)
+        {
+            m_transform.Rotate(m_transform.up, characterRotateSpeed * Time.deltaTime);
         }
     }
 }
