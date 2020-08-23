aa9e984522eaacd08edf373a2391f3e37411ade4
Chanhaeng Lee <lch32111@naver.com>
2020-08-23 14:09:06 +0900

    - fix : 더 이상 사용하지 않는 ChanTestScene 제거 - feat : Test Code 폴더추고, 3인칭 카메라 시스템 테스트를 위한 테스트 Scene 추가.             CharacterController Test Code추가

---
+++ CG_GAME_JAM_01/Assets/Code/_Test/TestCharacterController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCharacterController : MonoBehaviour
+{
+    private Transform m_transform;
+    private CharacterController m_characterController;
+
+    public float characterSpeed;
+
+    private void InitTestCharacterController()
+    {
+        m_transform = GetComponent<Transform>();
+        m_characterController = GetComponent<CharacterController>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitTestCharacterController();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_characterController.isGrounded == false)
+        {
+            m_characterController.Move(Physics.gravity * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.W) == true)
+        {
+            m_characterController.Move(transform.forward * Time.deltaTime * characterSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A) == true)
+        {
+            m_characterController.Move(transform.right * -1.0f * Time.deltaTime * characterSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S) == true)
+        {
+            m_characterController.Move(transform.forward * -1.0f * Time.deltaTime * characterSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D) == true)
+        {
+            m_characterController.Move(transform.right * Time.deltaTime * characterSpeed);
+        }
+    }
+}
