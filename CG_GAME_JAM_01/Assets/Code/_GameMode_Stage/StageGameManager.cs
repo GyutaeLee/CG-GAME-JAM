4946a8eabdbe5dc2ef490faec14b0c404027407e
GyutaeLee <gyutae07@gmail.com>
2020-08-30 19:06:41 +0900

    - feat : Stage 플레이용 게임 매니저 추가

--- CG_GAME_JAM_01/Assets/Code/_GameMode_Stage/StageGameManager.cs
+++ CG_GAME_JAM_01/Assets/Code/_GameMode_Stage/StageGameManager.cs
@@ -1,18 +1,90 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StageGameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum EGameState
     {
+        GAME_STATE_NONE = 0,
+
+        GAME_STATE_PLAYING,
+        GAME_STATE_THROWING,
+
+        GAME_STATE_PAUSE,
+        GAME_STATE_OVER,
+    }
+
+    private EGameState m_eGameState;
+
+    private StagePlayer m_stagePlayer;
+
+    private float m_gameCurrentTime;
+
+    private void Start()
+    {
+        //?? 규태 : 임시
+        Application.targetFrameRate = 60;
+
+        InitStageGameManager();
+        StartStageGame();
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdateStageGame();
+    }
+
+    private void InitStageGameManager()
+    {
+        this.m_stagePlayer = GameObject.Find("StagePlayer").GetComponent<StagePlayer>();
+        this.m_stagePlayer.SetPlayerViewThirdView();
+        this.m_stagePlayer.SetPlayerStateReady();
+
+        this.m_eGameState = EGameState.GAME_STATE_PAUSE;
+    }
+
+    private void StartStageGame()
+    {
+        this.m_eGameState = EGameState.GAME_STATE_PLAYING;
+    }
+
+    private void UpdateStageGame()
+    {
+        if (this.m_eGameState == EGameState.GAME_STATE_PAUSE || this.m_eGameState == EGameState.GAME_STATE_OVER)
+            return;
+
+        this.m_gameCurrentTime += Time.deltaTime;
+    }
+
+    /*
+     *  public
+     */
+    public void SetStageGameStatePlaying()
+    {
+        this.m_eGameState = EGameState.GAME_STATE_PLAYING;
+    }
+
+    public void SetStageGameStateThrowing()
+    {
+        this.m_eGameState = EGameState.GAME_STATE_THROWING;
+    }
+
+    public void SetStageGameStatePause()
+    {
+        this.m_eGameState = EGameState.GAME_STATE_PAUSE;
+    }
+
+    public void SetStageGameStateOver()
+    {
+        this.m_eGameState = EGameState.GAME_STATE_OVER;
+    }
+
+    //?? 규태 : 다른 스크립트로 옮기기
+    public void SceneMove(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }
